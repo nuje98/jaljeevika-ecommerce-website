406fb9eed7cec07fa92e0fe511b82946dc7d8c28
Anuj Jhamb <anuj.jhamb23@gmail.com>
2020-05-31 05:22:32 +0530

    User registration and login backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
 
 require('dotenv').config();
 
