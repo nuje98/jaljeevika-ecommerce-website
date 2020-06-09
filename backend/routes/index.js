a9a6b6d6ba64ff9bb62504b31731ee7acd842201
Anuj Jhamb <anuj.jhamb23@gmail.com>
2020-06-09 15:27:13 +0530

    drop-down issue reemoved

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+let Product = require('../models/product.model');
 
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
 // Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
-  res.render('dashboard', {
-    user: req.user
-  })
-);
+router.route('/dashboard').get((req, res) => { 
+  Product.find({}, function(err, data){
+      res.render('dashboard.ejs', { 
+         products : data
+      });
+  });
+});
 
 // Products Page
 router.get('/product', ensureAuthenticated, (req, res) => res.render('product'));
